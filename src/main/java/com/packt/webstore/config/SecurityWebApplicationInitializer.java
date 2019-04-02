e11fc26de91d3afa2529d3faffee74db469cbcf8
Chitra Limbu <limbu_nikkil@hotmail.co.uk>
2019-04-02 22:22:49 +0100

    logout button added

--- src/main/java/com/packt/webstore/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/packt/webstore/config/SecurityWebApplicationInitializer.java
@@ -1,5 +1,7 @@
 package com.packt.webstore.config;
 
-public class SecurityWebApplicationInitializer {
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+	
 }
